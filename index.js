8af061fcdba611cc8d41dbe122eb6fb5442c9bae
MS Rahman <msrahman048gmail.com>
2024-08-23 22:41:38 +0600

    last comment

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      "https://msr-store-1b036.web.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
@@ -26,19 +37,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const menuCollection = client.db("bistroDb").collection("menu");
-    const reviewCollection = client.db("bistroDb").collection("reviews");
+    const productsCollection = client.db("msrStore").collection("products");
 
-    app.get('/menu', async(req, res) =>{
-        const result = await menuCollection.find().toArray();
+    app.get('/products', async(req, res) =>{
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+        const result = await productsCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
         res.send(result);
     })
-    
-    app.get('/reviews', async(req, res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+    app.get('/productsCount', async(req, res) =>{
+        const count = await productsCollection.estimatedDocumentCount();
+        res.send({count});
     })
 
     // Send a ping to confirm a successful connection
@@ -53,9 +67,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
+    res.send('msrStore')
 })
 
 app.listen(port, () => {
-    console.log(`Bistro boss is sitting on port ${port}`);
+    console.log(`msrStore on port ${port}`);
 })
